2a57a2b00b7244f80b8181618e875a0b085e85de
Thibault Saunier <tsaunier@gnome.org>
2010-08-10 11:30:09 -0400

    Adapt etree test to pass with the latest effect saving implementation

--- tests/test_projectmanager.py
+++ tests/test_projectmanager.py
@@ -127,7 +127,7 @@ class TestProjectManager(TestCase):
 
             def _loadProject(self, location, project=None):
                 raise FormatterError()
-        self.manager._getFormatterForUri = lambda uri: FailFormatter()
+        self.manager._getFormatterForUri = lambda uri: FailFormatter([])
 
         uri = "file:///Untitled.xptv"
         self.manager.loadProject(uri)
@@ -164,7 +164,7 @@ class TestProjectManager(TestCase):
 
             def _fillTimeline(self):
                 pass
-        self.manager._getFormatterForUri = lambda uri: MissingUriFormatter()
+        self.manager._getFormatterForUri = lambda uri: MissingUriFormatter([])
 
         uri = "file:///Untitled.xptv"
         self.manager.loadProject(uri)
@@ -199,7 +199,7 @@ class TestProjectManager(TestCase):
 
             def _fillTimeline(self):
                 pass
-        self.manager._getFormatterForUri = lambda uri: EmptyFormatter()
+        self.manager._getFormatterForUri = lambda uri: EmptyFormatter([])
 
         uri = "file:///Untitled.xptv"
         self.manager.loadProject(uri)
